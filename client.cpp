993822b4826462b75d5dfe9df50852315bc3d536
jim0112 <58672813+jim0112@users.noreply.github.com>
2022-01-14 09:25:08 +0800

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,266 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <string>
+#include <sys/stat.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include "http_parser.h"
+#include <algorithm>
+#include <filesystem>
+
+#define BUFF_SIZE 1024
+#define ERR_EXIT(a){ perror(a); exit(1); }
+using namespace std;
+int init_client(char* ip_port, struct sockaddr_in addr){
+    int sockfd;
+    char ip[32] = {'\0'};
+    char port[10] = {'\0'};
+    int flag = 0;
+    int t = 0;
+    for(int i = 0; i < strlen(ip_port); i++){
+        if(ip_port[i] == ':')
+            flag = 1;
+        else if(flag == 0)
+            ip[i] = ip_port[i];
+        else{
+            port[t] = ip_port[i];
+            t++;
+        }
+    }
+    // Get socket file descriptor
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        ERR_EXIT("socket failed\n");
+    }
+    // Set server address
+    bzero(&addr,sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(atoi(port));
+    //printf("%s %s\n", ip, port);
+    // Connect to the server
+    if(connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0){
+        ERR_EXIT("connect failed\n");
+    }
+    return sockfd;
+}
+int init_server(int port, struct sockaddr_in &server_addr){
+    int server_sockfd;
+    if((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        ERR_EXIT("socket failed\n")
+    }
+    int opt = 1;
+    if (setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))){
+        ERR_EXIT("setsockopt failed");
+    }
+    // Set server address information
+    bzero(&server_addr, sizeof(server_addr)); // clear the data
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(port);
+    
+    // Bind the server file descriptor to the server address
+    if(::bind(server_sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
+        ERR_EXIT("bind failed\n");
+    }
+    // Listen on the server file descriptor
+    if(listen(server_sockfd , 10) < 0){
+        ERR_EXIT("listen failed\n");
+    }
+    return server_sockfd;
+}
+int main(int argc, char* argv[]){
+    struct sockaddr_in cli_addr, serv_addr;
+    char buffer[BUFF_SIZE] = {};
+    // one for communicating with server(as client), one for recieving browser's request(as server)
+    int toserver_sockfd = init_client(argv[1], cli_addr);
+    int tobrowser_sockfd = init_server(atoi(argv[2]), serv_addr);
+    int browser_sockfd;
+    bool browser_connected = 0;
+    cout << "\033[1;36mFinish Initialization...\033[1;36m" << endl;
+    // start handling browser request...
+    //fd_set readfds;
+    bool log_in = false;
+    while(1){
+        /*
+        FD_ZERO(&readfds);
+        FD_SET(toserver_sockfd, &readfds);
+        int max_sd;
+        if(browser_connected){
+            FD_SET(browser_sockfd, &readfds);
+            max_sd = max(toserver_sockfd, browser_sockfd);
+        }else{
+            FD_SET(tobrowser_sockfd, &readfds);
+            max_sd = max(toserver_sockfd, tobrowser_sockfd);
+        }
+        int activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL); 
+        if ((activity < 0) && (errno!=EINTR))printf("select error"); 
+        */
+        // accept browser connection
+        if(!browser_connected){
+            int addrlen = sizeof(serv_addr);
+            if((browser_sockfd = accept(tobrowser_sockfd, (struct sockaddr *) &serv_addr, (socklen_t*) &addrlen)) < 0){
+                ERR_EXIT("accept failed\n");
+            }
+            browser_connected = true;
+            cout << "\033[1;32mBrowser Connect Success\033[1;32m" << endl;
+        }
+        if(browser_connected){
+            int n;
+            char buffer[BUFF_SIZE];
+            // check if browser disconnects
+            if((n = recv(browser_sockfd, buffer, 1, MSG_PEEK | MSG_DONTWAIT)) == 0){
+                close(browser_sockfd);
+                browser_connected = 0;
+                cout << "\033[1;31mBrowser Disconnect\033[0m" << endl;
+            }
+            // check the http request
+            else{
+                http_request req = get_http_request(browser_sockfd);
+                cout << "\033[1;31mGet Request\033[0m" << endl;
+                req.display();
+                if(req.url.substr(0, 7) == "/image/"){
+                    send_http_response(browser_sockfd, "." + req.url);
+                }
+                else if(req.url == "/favicon.ico"){
+                    send_http_response(browser_sockfd, "./image/ig_favicon.ico");
+                }
+                else{
+                    if(!log_in){
+                        if(req.method == "POST"){
+                            // implemnt login (message)
+                            if(req.url == "/login"){
+                                //cout << req.content << endl;
+                                map<string, string> tmp = parse_http_content(req.content);
+                                //send instr + username
+                                cout << "\033[1;32mReceive Username: \033[0m" << tmp["username"] << endl;
+                                send_to_server(toserver_sockfd, "LOGIN " + tmp["username"]);
+                                string ce = recv_from_server(toserver_sockfd);
+                                if(ce == "OK"){
+                                    cout << "\033[1;32mLogin Success\033[0m" << endl;
+                                    log_in = true;    
+                                }
+                                else{
+                                    cout << "\033[1;32mLogin Fail\033[0m" << endl;
+                                }
+                                http_redirect(browser_sockfd, "/");
+                            }
+                            else{
+                                cout << "\033[1;32mUndefined Operation\033[0m" << endl;
+                            }
+                        }
+                        else if(req.method == "GET"){
+                            // implement login (interface -> image)
+                            if(req.url.substr(0,7) == "/basic/"){
+                                send_http_response(browser_sockfd, "." + req.url);
+                            }
+                            else{
+                                send_http_response(browser_sockfd, "./basic/login.html");
+                            }
+                        }
+                    }
+                    else{
+                        if(req.method == "POST"){
+                            // implement add friend, del friend, send message to friend
+                            if(req.url == "/add"){
+                                map<string, string> tmp = parse_http_content(req.content);
+				cout << tmp["username"] << endl;
+                                send_to_server(toserver_sockfd, "ADD " + tmp["username"]);
+                                string ce = recv_from_server(toserver_sockfd);
+                                if(ce == "OK"){
+                                    cout << "\033[1;32mAdd Friend Success\033[0m" << endl;
+                                }
+                                else{
+                                    cout << "\033[1;32mAdd Friend Fail\033[0m" << endl;
+                                }
+                                http_redirect(browser_sockfd, "/");
+                            }
+                            else if(req.url == "/remove"){
+                                map<string, string> tmp = parse_http_content(req.content);
+                                send_to_server(toserver_sockfd, "REMOVE " + tmp["username"]);
+                                string ce = recv_from_server(toserver_sockfd);
+                                if(ce == "OK"){
+                                    cout << "\033[1;32mRemove Friend Success\033[0m" << endl;
+                                }
+                                else{
+                                    cout << "\033[1;32mRemove Friend Fail\033[0m" << endl;
+                                }
+                                http_redirect(browser_sockfd, "/");
+                            }
+                            else if(req.url == "/send_mess"){
+                                // sending message to others
+                                map<string, string> tmp = parse_http_content(req.content);
+                                send_to_server(toserver_sockfd, "CHAT " + tmp["recver"]);
+                                send_to_server(toserver_sockfd, tmp["content"]);
+                                string ce = recv_from_server(toserver_sockfd);
+                                if(ce == "OK"){
+                                    cout << "\033[1;32mSending(Mess) Success\033[0m" << endl;
+                                }
+                                else{
+                                    cout << "\033[1;32mSending(Mess) Fail\033[0m" << endl;
+                                }
+                                http_redirect(browser_sockfd, "/chat/" + tmp["recver"]);
+                            }
+                            else if(req.url == "/send_image"){
+                                // sending image
+                                map<string, string> tmp = parse_http_content(req.content);
+                                send_to_server(toserver_sockfd, "PUT " + tmp["recver"] + "IMAGE");
+                                send_to_server_file(toserver_sockfd, tmp["file"].c_str());
+                                string ce = recv_from_server(toserver_sockfd);
+                                if(ce == "OK"){
+                                    cout << "\033[1;32mSending(Image) Success\033[0m" << endl;
+                                }
+                                else{
+                                    cout << "\033[1;32mSending(Image) Fail\033[0m" << endl;
+                                }
+                                http_redirect(browser_sockfd, "/chat/" + tmp["recver"]);
+                            }
+                            else if(req.url == "/send_file"){
+                                // sending file
+                                map<string, string> tmp = parse_http_content(req.content);
+                                send_to_server(toserver_sockfd, "PUT " + tmp["recver"] + "FILE");
+                                send_to_server_file(toserver_sockfd, tmp["file"].c_str());
+                                string ce = recv_from_server(toserver_sockfd);
+                                if(ce == "OK"){
+                                    cout << "\033[1;32mSending(File) Success\033[0m" << endl;
+                                }
+                                else{
+                                    cout << "\033[1;32mSending(File) Fail\033[0m" << endl;
+                                }
+                                http_redirect(browser_sockfd, "/chat/" + tmp["recver"]);
+                            }
+                        }
+                        else if(req.method == "GET"){
+                            // implement open chatroom, list friend, click file
+                            if(req.url == "/friends"){
+                                send_to_server(toserver_sockfd, "LIST");
+                                string temp = recv_from_server(toserver_sockfd);
+                                ofstream temp_fstream;
+                                temp_fstream.open("./data/friends.json");
+                                temp_fstream << temp;
+                                temp_fstream.close();
+                                cout << "send friend list to browser" << endl;
+                                send_http_response(browser_sockfd, "./data/friends.json");
+                            }
+                            else if(req.url.substr(0,6) == "/chat/"){
+                                send_http_response(browser_sockfd, "./basic/chatroom.html");
+                            }
+                            else if(req.url.substr(0,6) == "/view/"){
+                                // temporarily left unsolved
+                            }
+                            else{
+                                // redirect
+                                send_http_response(browser_sockfd, "./basic/main.html");
+                                // <script> -> /friends -> append chatroom
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
---
+++ http_parser.cpp
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <string>
+#include <sys/stat.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <filesystem>
+#include "http_parser.h"
+#define BUFF_SIZE 1024
+using namespace std;
+
+void http_request::display(){
+    cout << "========START OF REQUEST========\n";
+    cout << "METHOD: " << method <<"\nACTION: " << url << "\nVERSION: " << version << "\nHEADERS: \n";
+    for (std::map<string,string>::iterator it = headers.begin(); it != headers.end(); it++){
+        cout << it->first << " => " << it->second << endl;
+    }
+    cout << "CONTENT:\n" << content << endl;
+    cout << "========END OF REQUEST========\n";
+}
+http_request get_http_request(int sockfd){
+    http_request req;
+    char buffer[BUFF_SIZE];
+    string line = "";
+    int flag = 0;
+    bool first_line = true;
+    while(1){
+        int n = read(sockfd, buffer, 1);
+        if(n <= 0)
+            break;
+        if(buffer[0] == '\n'){
+            line = "";
+        }
+        else if(buffer[0] != '\r'){
+            line += buffer[0];
+        }
+        else if(buffer[0] == '\r'){
+            if(line == ""){
+                // got the last \r -> finished
+                // read \n
+                int n = read(sockfd, buffer, 1);
+                break;
+            }
+            if(first_line){
+                stringstream i_ss(line);
+                i_ss >> req.method >> req.url >> req.version;
+                first_line = false;
+            }
+            else{
+                string key, value;
+                int cut = line.find(':');
+                key = line.substr(0, cut);
+                value = line.substr(cut + 2);
+                req.headers.insert(pair<string, string>(key, value));
+            }
+        }
+    }
+    if(req.headers.count("Content-Length") != 0){
+        int length = stoi(req.headers["Content-Length"]);
+        //cout << length << endl;
+        string content = "";
+        while((int)content.length() < length){
+            bzero(buffer, sizeof(buffer));
+            int n = read(sockfd, buffer, BUFF_SIZE);
+            //buffer[n] = '\0';
+            //printf("%s\n", buffer);
+            content += string(buffer);
+        }
+        req.content = content;
+    }
+    else{
+        req.content = "";
+    }
+    return req;
+}
+
+void send_http_response(int sockfd, string filename){
+    FILE *fp;
+    fp = fopen(filename.c_str(), "rb");
+    // check file's size
+    struct stat buf;
+    stat(filename.c_str(), &buf);
+    int size = (int)buf.st_size;
+    string response = "HTTP/1.1 200 OK\r\nContent-Length: " + to_string(size) + "\r\n\r\n";
+    int bytes_sent = 0;
+    // send http response
+    while(bytes_sent < response.length()){
+        int n = write(sockfd, response.substr(bytes_sent).c_str(), response.length() - bytes_sent);
+        bytes_sent += n;
+    }
+    // send corresponding file
+    char buffer[BUFF_SIZE];
+    bytes_sent = 0;
+    int bytes_read;
+    while(bytes_sent < size){
+        if((bytes_read = fread(buffer, 1, BUFF_SIZE, fp)) <= 0){
+            break;
+        }
+        size_t cur_bytes_sent = 0;
+        while(cur_bytes_sent < bytes_read){
+            int n = write(sockfd, buffer + cur_bytes_sent, bytes_read - cur_bytes_sent);
+            if(n == -1){
+                fclose(fp);
+                return;
+            }
+            cur_bytes_sent += n;
+        }
+        bytes_sent += cur_bytes_sent;
+        bzero(buffer, sizeof(buffer));
+    }
+    fclose(fp);
+    return;
+}
+void http_redirect(int sockfd, string root){
+    string http_str = "HTTP/1.1 303 See Other\r\nLocation: " + root + "\r\n\r\n";
+    int cnt = 0;
+    while(cnt < http_str.length()){
+        int n = write(sockfd, http_str.substr(cnt).c_str(), http_str.length() - cnt);
+        cnt += n;
+    }
+}
+
+void send_to_server(int sockfd, string instr){
+    write(sockfd, instr.c_str(), BUFF_SIZE);
+    return;
+}
+string recv_from_server(int sockfd){
+    int size;
+    read(sockfd, &size, sizeof(size));
+    cout << "Friend size is : " << size << endl;
+    int cnt = 0;
+    char buffer[BUFF_SIZE];
+    string result = "";
+    while(cnt < size){
+        bzero(buffer, sizeof(buffer));
+        int n = read(sockfd, buffer, size - cnt);
+        cnt += n;
+        result += string(buffer);
+    }
+    cout << result << endl;
+    return result;
+}
+void send_to_server_file(int sockfd, const char* filename){
+    FILE *fp = fopen(filename, "rb");
+    int size;
+    struct stat st;
+    stat(filename, &st);
+    size = (int)st.st_size;
+    write(sockfd, &size, sizeof(size));
+    int sent_cnt = 0;
+    int cur_bytes_read;
+    char buffer[BUFF_SIZE];
+    while(sent_cnt < size){
+        if((cur_bytes_read = fread(buffer, 1, sizeof(buffer), fp)) <= 0){
+            break;
+        }
+        size_t cur_bytes_sent = 0;
+        while(cur_bytes_sent < cur_bytes_read){
+            int n = write(sockfd, buffer + cur_bytes_sent, cur_bytes_read - cur_bytes_sent);
+            if(n == -1){
+                fclose(fp);
+                return;
+            }
+            cur_bytes_sent += n;
+        }
+        sent_cnt += cur_bytes_sent;
+        bzero(buffer, sizeof(buffer));
+    }
+    fclose(fp);
+}
+void recv_from_server_file(int sockfd, const char* filename){
+    FILE *fp = fopen(filename, "wb");
+    int size;
+    read(sockfd, &size, sizeof(size));
+    int cnt = 0;
+    char buffer[BUFF_SIZE];
+    while(cnt < size){
+        int n = read(sockfd, buffer, min(sizeof(buffer), (long unsigned int)(size - cnt)));
+        cnt += n;
+        fwrite(buffer, 1, n, fp);
+        bzero(buffer, sizeof(buffer));
+    }
+    fclose(fp);
+}
+map<string, string> parse_http_content(string content){
+    map<string, string> ret;
+    int begin = 0, end = 0;
+    while(end <= content.length()){
+        if(end == content.length() || content[end] == '&'){
+            string entry = content.substr(begin, end - begin);
+            int eq = entry.find("=");
+            //cout << entry.substr(0, eq) << " " << entry.substr(eq+1) << endl;
+            ret.insert(make_pair(entry.substr(0, eq), entry.substr(eq + 1)));
+            begin = end + 1;
+            //cout << ret.count("username") << endl;
+        }
+        end += 1;
+    }
+    return ret;
+}
